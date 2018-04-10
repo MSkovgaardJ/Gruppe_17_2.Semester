c41d9d4fac5fd0a87fc128b2a88d472b7cda7574
StephanieNigth <s.hvenegaard@outlook.com>
2018-04-10 13:31:48 +0200

    new thing

--- src/sensumudred/SensumUdred.java
+++ src/sensumudred/SensumUdred.java
@@ -16,6 +16,7 @@ public class SensumUdred {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Group 17 is awesome");
     }
     
 }
