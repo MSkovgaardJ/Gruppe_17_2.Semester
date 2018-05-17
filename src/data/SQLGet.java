c8c569a409ac979d475ef825d3e2aa7ef4ba1d82
MSkovgaardJ <mojen17@student.sdu.dk>
2018-05-17 10:49:04 +0200

    Creating of getJournal og getAid

--- src/data/SQLGet.java
+++ src/data/SQLGet.java
@@ -14,14 +14,16 @@ public class SQLGet
     public static String getAllCitizens = "select * from Citizen";
     public static String getAllJournals = "select * from Journal";
     public static String getAllAids = "select * from Aid";
-    public static String getCitizen(int ssn){
-       return "select * from Citizen where SSS="+ssn;
+    public static String getCitizen(int ssn)
+    {
+       return "select * from Citizen where SSN="+ssn;
     }  
-    public static String getJournal(int jno){
-        return null;
+    public static String getJournal(int jno)
+    {
+        return "select * from Journal where JournalNumber=" + jno;
     }    
     public static String getAid(int aidno){
-        return null;
+        return "select * from Aid where AidNumber =" + aidno;
     }    
     public static String checklogin(String username,String password){
         return "select Username, Password from systemuser where username = '" + username + "' and password = '" + password + "'";
